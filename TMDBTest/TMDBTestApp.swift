a61bddc715d5e81df479df39273d8870db8a6e6d
Mario Vanegas <mvanegas@applaudostudios.com>
2024-03-15 12:45:14 -0600

    Initial Commit

---
+++ TMDBTest/TMDBTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TMDBTestApp.swift
+//  TMDBTest
+//
+//  Created by Mario Vanegas on 15/3/24.
+//
+
+import SwiftUI
+
+@main
+struct TMDBTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
